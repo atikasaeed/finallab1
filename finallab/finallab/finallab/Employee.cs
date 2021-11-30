23a18c07d333fa08141adbd004d5fe9483ce6f2e
atikasaeed <93571930+atikasaeed@users.noreply.github.com>
2021-11-30 12:27:44 +0500

    Add files via upload

---
+++ finallab/finallab/finallab/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab
+{
+    public class Employee
+    {
+        public string EmployeeID()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public String EmployeeName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string EmployeeType()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Int32 Hoursworked()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Int32 NoofUnit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Int32 Hourlyrate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void calculatesalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ finallab/finallab/finallab/hourlyemployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab
+{
+    public class hourlyemployee : Employee
+    {
+        public void calculatesalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
